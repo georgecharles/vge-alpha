96413c9e100dab34f1fbf8d717bc3bd2f68a4507
georgecharles <georgehorrobin@outlook.com>
2025-02-21 01:15:26 +0000

    Initial commit from your app

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,20 +1,20 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+    const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+    const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-console.log("Supabase Anon Key:", supabaseAnonKey); // Add this line
+    console.log("Supabase Anon Key:", supabaseAnonKey); // Add this line
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Missing Supabase environment variables");
-}
+    if (!supabaseUrl || !supabaseAnonKey) {
+      throw new Error("Missing Supabase environment variables");
+    }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    persistSession: true,
-    storageKey: "my-vge-auth",
-    storage: window.localStorage,
-    autoRefreshToken: true,
-    detectSessionInUrl: false, // set to false
-  },
-});
+    export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+      auth: {
+        persistSession: true,
+        storageKey: "my-vge-auth",
+        storage: window.localStorage,
+        autoRefreshToken: true,
+        detectSessionInUrl: false, // set to false
+      },
+    });
